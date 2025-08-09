d83688aa2df3267e7af9e4872b4170ab0804f3ce
hazrath15 <mohammadhazrathalicse15@gmail.com>
2025-08-09 10:17:26 +0600

    add uninstall functionality

--- uninstall.php
+++ uninstall.php
@@ -3,4 +3,22 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
+$portfolio_posts = get_posts([
+    'post_type'      => 'sp_portfolio',
+    'posts_per_page' => -1,
+    'fields'         => 'ids',
+]);
+
+$portfolio_meta_keys = [
+    '_sp_client_name',
+    '_sp_project_url',
+];
+
+foreach ($portfolio_posts as $post_id) {
+    foreach ($portfolio_meta_keys as $key) {
+        delete_post_meta($post_id, $key);
+    }
+    wp_delete_post($post_id, true);
+}
+
 ?>
